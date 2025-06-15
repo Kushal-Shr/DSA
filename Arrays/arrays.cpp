129e5783b5c1f471b4a1b2104f8beb1cf4e7ddc1
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-06-15 18:40:39 -0500

    Array.txt-correction

---
+++ Arrays/arrays.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // Creation of an array
+    int marks[5] = {99, 95, 96, 91, 93}; // Array creation with initialization
+
+    // Alternatively, you can also use:
+    // int marks[] = {99, 95, 96, 91, 93}; Array creation without specifying size
+}
