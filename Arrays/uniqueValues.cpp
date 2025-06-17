9a50beb207662a25af74dbb5ae122498e89f3710
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-06-16 23:23:22 -0500

    unique-values

---
+++ Arrays/uniqueValues.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int arr[] = {1, 2, 5, 3, 1, 2, 3, 4};
+    int size = 8;
+
+    for (int i = 0; i < size; i++)
+    {
+        int count = 0;
+
+        for (int j = 0; j < size; j++)
+        {
+            if (arr[i] == arr[j])
+            {
+                count++;
+            }
+        }
+        if (count == 1)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+
+    return 0;
+}
