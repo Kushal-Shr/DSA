a01f5c0be403f505d5b47071b12180c0b0440509
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-07-12 00:51:41 -0500

    pattern2

--- pattern/pattern2.cpp
+++ pattern/pattern2.cpp
@@ -1,7 +1,6 @@
-// * * * *
-// * * * *
-// * * * *
-// * * * *
+// 1 1 1
+// 2 2 2
+// 3 3 3
 
 #include <iostream>
 
@@ -9,16 +8,18 @@ using namespace std;
 
 int main()
 {
-    int row = 4;
-    int column = 4;
+    int row = 3;
+    int col = 3;
 
     int i = 0;
+
     while (i < row)
     {
         int j = 0;
-        while (j < column)
+
+        while (j < col)
         {
-            cout << "* ";
+            cout << i + 1 << " ";
             j++;
         }
 
