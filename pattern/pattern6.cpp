0d689f94c6d45183a17ecdd01e9d5f696cd3fd29
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-07-12 16:25:59 -0500

    pattern6

---
+++ pattern/pattern6.cpp
@@ -0,0 +1,31 @@
+// 1
+// 2 1
+// 3 2 1
+// 4 3 2 1
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int row = 5;
+
+    int i = 1;
+
+    while (i < row)
+    {
+        int j = 1;
+
+        while (j <= i)
+        {
+            cout << i - j + 1 << " ";
+            j++;
+        }
+        cout << endl;
+
+        i++;
+    }
+
+    return 0;
+}
