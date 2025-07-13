349a72ecb34b2c4402e4cdbd338ce0950c04372c
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-07-13 16:37:53 -0500

    sumProblem

---
+++ loopProblems/sum.cpp
@@ -0,0 +1,20 @@
+// prints the sum of all numbers from 1 to n
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n = 10;
+    int sum = 0;
+
+    for (int i = 1; i <= n; i++)
+    {
+        sum += i;
+    }
+
+    cout << sum;
+
+    return 0;
+}
