303c1533c9d3c6627ba13020d22d520758c0ea54
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-07-27 18:35:33 -0500

    priorityqueue

---
+++ STL/priorityQueue.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    // max heap
+    priority_queue<int> maxi;
+
+    maxi.push(1);
+    maxi.push(5);
+    maxi.push(7);
+    maxi.push(4);
+
+    int n = maxi.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << maxi.top() << " ";
+        maxi.pop();
+    }
+    cout << endl;
+
+    // min heap
+    priority_queue<int, vector<int>, greater<int>> mini;
+
+    mini.push(1);
+    mini.push(5);
+    mini.push(7);
+    mini.push(4);
+
+    int m = mini.size();
+    for (int i = 0; i < n; i++)
+    {
+        cout << mini.top() << " ";
+        mini.pop();
+    }
+    cout << endl;
+
+    return 0;
+}
