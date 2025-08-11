25d79249e135040e90e58bf9318d92d91bb5cbe7
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-08-10 23:53:54 -0500

    count

---
+++ recursion/counting.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+void print(int n)
+{
+    if (n == 0)
+        return;
+
+    cout << n << endl;
+
+    print(n - 1);
+}
+
+int main()
+{
+    cout << "Enter a number: ";
+    int num;
+    cin >> num;
+
+    print(num);
+
+    return 0;
+}
