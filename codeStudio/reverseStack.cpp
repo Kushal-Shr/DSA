a3461c9baa74ad132d481aac9c197da767e07945
Kushal Shrestha <whotfisksl.17@gmail.com>
2025-10-06 20:40:41 -0500

    reverse-stack

---
+++ codeStudio/reverseStack.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+void insertAtBottom(stack<int> &st, int x)
+{
+    if (st.empty())
+    {
+        st.push(x);
+        return;
+    }
+
+    int num = st.top();
+    st.pop();
+
+    insertAtBottom(st, x);
+
+    st.push(num);
+}
+
+void reverseStack(stack<int> &st)
+{
+    if (st.empty())
+        return;
+
+    int num = st.top();
+    st.pop();
+
+    reverseStack(st);
+
+    insertAtBottom(st, num);
+}
+
+int main()
+{
+    stack<int> st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+    st.push(5);
+
+    cout << st.top() << endl;
+
+    reverseStack(st);
+
+    cout << st.top() << endl;
+
+    return 0;
+}
